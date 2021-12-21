76d695f1da2024dcbb9c2152e64a7d1b09e6caa0
Sayuris1 <elgunalp1@gmail.com>
2021-12-21 03:51:57 +0300

    init

---
+++ About.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Data.OleDb;
+
+namespace asp
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
